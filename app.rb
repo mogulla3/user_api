4eb24ac68abe59b9aaa04df9513151bf6618700d
Hiroki Sanpei <hs.iarn3@gmail.com>
2014-05-04 00:20:18 +0900

    Update app.rb
    単一ユーザー取得APIの実装完了(1st)
    - fields指定時の実装

--- app.rb
+++ app.rb
@@ -14,8 +14,26 @@ get '/users/:id' do
   user = users.detect {|user| user['id'] == params[:id].to_i}
 
   unless user
-    logger.info({'status' => 400, 'message' => "user_id '#{params[:id]}' is not found", 'params' => params})
-    return json({'status' => 400, 'message' => "user_id '#{params[:id]}' is not found"})
+    logger.info({'status' => 400, 'message' => "User-id `#{params[:id]}` is not found.", 'params' => params})
+    return json({'status' => 400, 'message' => "User-id `#{params[:id]}` is not found."})
+  end
+
+  if params[:fields]
+
+    fields = params[:fields].split(',')
+    logger.debug user
+    logger.debug fields
+
+    # 存在しないfieldが指定された場合、エラーを返す
+    fields.each do |field|
+      if user.has_key?(field) == false
+        logger.info({'status' => 400, 'message' => "Fields `#{field}` is invalid field.", 'params' => params})
+        return json({'status' => 400, 'message' => "Fields `#{field}` is invalid field."})
+      end
+    end
+
+    return json(user.select {|key| fields.include? key})
+
   end
 
   logger.info({'status' => 200, 'message' => 'success', 'params' => params})
