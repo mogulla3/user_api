fcf84b85fa4066eb2a7857ac7eaddf74321f375e
Hiroki Sanpei <hs.iarn3@gmail.com>
2014-05-04 14:37:59 +0900

    Update app.rb
    - 存在しないfieldsが指定された場合にエラーを返さない仕様に変更
    - 複数ユーザー参照APIの実装
    - ハッシュを使う場面ではキーにシンボルを使うように変更

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'logger'
 require 'sinatra/reloader'
 Bundler.require
 
-users  = YAML.load_file('userdata.yml')
+USERS  = YAML.load_file('userdata.yml')
 logger = Logger.new('logs/user_api.log')
 logger.level = Logger::DEBUG
 
@@ -11,39 +11,50 @@ get '/users/:id' do
 
   # 指定されたidのユーザーを取得する
   # 存在しない場合、detectはnilを返す
-  user = users.detect {|user| user['id'] == params[:id].to_i}
+  user = USERS.detect {|user| user['id'] == params[:id].to_i}
 
   unless user
-    logger.info({'status' => 400, 'message' => "User-id `#{params[:id]}` is not found.", 'params' => params})
-    return json({'status' => 400, 'message' => "User-id `#{params[:id]}` is not found."})
+    logger.info({error_code: 'u-100', status_code: 400, message: "User-id `#{params[:id]}` is invalid.", params: params})
+    status 400
+    return json({error_code: 'u-100', status_code: 400, message: "User-id `#{params[:id]}` is invalid."})
   end
 
   if params[:fields]
-
     fields = params[:fields].split(',')
-    logger.debug user
-    logger.debug fields
 
-    # 存在しないfieldが指定された場合、エラーを返す
-    fields.each do |field|
-      if user.has_key?(field) == false
-        logger.info({'status' => 400, 'message' => "Fields `#{field}` is invalid field.", 'params' => params})
-        return json({'status' => 400, 'message' => "Fields `#{field}` is invalid field."})
-      end
-    end
-
+    status 200
+    logger.info({status_code: 200, message: 'success', params: params})
     return json(user.select {|key| fields.include? key})
-
   end
 
-  logger.info({'status' => 200, 'message' => 'success', 'params' => params})
+  logger.info({status_code: 200, message: 'success', params: params})
+  status 200
   json(user)
 
 end
 
 # 複数ユーザー情報の取得
-get '/users/' do 
-  'under construction' 
+# TODO: ログにリクエスト時のURL残したい
+get '/users' do
+
+  # id指定がされている場合、該当するidのみを抽出する
+  if params[:id]
+    userid_list = params[:id].split(',')
+    users = USERS.select {|user| userid_list.include?(user['id'].to_s)}
+  end
+
+  if params[:fields]
+    fields = params[:fields].split(',')
+
+    status 200
+    logger.info({status_code: 200, message: 'success', params: params})
+    return json(users.map {|user| user.select {|key,_| fields.include? key}})
+  end
+
+  logger.info({status_code: 200, message: 'success', params: params})
+  status 200
+  json(users)
+
 end
 
 # ユーザー情報の新規作成
